277ca84c97a5ce8c428b2470a4e40d728db85aef
Jörg Thalheim <joerg@thalheim.io>
2022-09-22 09:47:22 +0200

    package polar properly

--- Procfile
+++ Procfile
@@ -1,4 +1,5 @@
-bitcoind: bitcoind  -fallbackfee=0.0002 -server -rpcuser=polaruser -rpcpassword=polarpass -chain=regtest
-sensei: bash ./setup-sensei && cargo run --bin senseid -- \
-  --network=regtest --bitcoind-rpc-host=localhost --bitcoind-rpc-port=18443 --bitcoind-rpc-username=polaruser --bitcoind-rpc-password=polarpass --database-url=sensei.db
-fund-wallet: bash ./setup-wallet && sleep 99999999
+# TODO for now we use polar to setup the network
+#bitcoind: bitcoind  -fallbackfee=0.0002 -server -rpcuser=polaruser -rpcpassword=polarpass -chain=regtest
+#fund-wallet: bash ./setup-wallet && sleep 99999999
+sensei: bash ./setup-sensei && cargo run --bin senseid -- --network=regtest --bitcoind-rpc-host=localhost --bitcoind-rpc-port=18446 --bitcoind-rpc-username=polaruser --bitcoind-rpc-password=polarpass --database-url=sensei.db
+polar: polar
--- default.nix
+++ default.nix
@@ -1,4 +1,22 @@
- with import <nixpkgs> {};
+with import <nixpkgs> {};
+let
+  version = "1.4.0";
+  pname = "polar";
+  src = fetchurl {
+    url = "https://github.com/jamaljsr/polar/releases/download/v${version}/polar-linux-x86_64-v${version}.AppImage";
+    sha256 = "sha256-m6WR/PUaoC0pArvIxwdJZNuwo0oNwEofReXG+CS92YQ=";
+  };
+  appimageContents = appimageTools.extractType2 { inherit pname src version; };
+
+  polar = appimageTools.wrapType2 {
+    inherit pname src version;
+    extraInstallCommands = ''
+      mv $out/bin/polar-${version} $out/bin/polar
+      . ${makeWrapper}/nix-support/setup-hook
+      wrapProgram "$out/bin/polar" --prefix PATH : ${docker-compose}/bin
+    '';
+  };
+in
  mkShell {
    nativeBuildInputs = [
      bashInteractive
@@ -7,8 +25,10 @@
      rustfmt
      protobuf
      nodejs
-     bitcoind
+     #bitcoind
+     #bitcoin
      hivemind
+     polar
    ];
    # this package bundles static protoc binaries
    # This line requires: programs.nix-ld.enable = true; in your nixos configuration
